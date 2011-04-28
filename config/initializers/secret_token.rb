f845c007cda580f1a97c154a9f4f81b5cd2b3215
Oleg <oleg@theworkinggroup.ca>
2011-04-28 19:33:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SayAnything::Application.config.secret_token = '75a7d676e2ee7ec30b77e69f71468240409dd4a93eeee2c7bf0f804f570a06363e9325104431ecb9a520da57693b304fbd103fcf5469dfeb280d5ce0af46f722'
